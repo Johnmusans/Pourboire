0a2b1d015710b84a08ea8a34e162ccd12596478e
mwka20 <21mk406@esisalama.org>
2024-05-18 09:29:01 +0300

    phase 5:  Composition

--- app/src/main/java/cd/jenovicdev/pourboire/MainActivity.kt
+++ app/src/main/java/cd/jenovicdev/pourboire/MainActivity.kt
@@ -1,5 +1,6 @@
 package cd.jenovicdev.pourboire
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,6 +19,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -44,12 +46,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    val amountInput = "0"
+    var amountInput = mutableStateOf("0")
     TextField(
-        value = amountInput,
-        onValueChange = {},
+        value = amountInput.value,
+        onValueChange = { amountInput.value = it},
         modifier = modifier
     )
 }
